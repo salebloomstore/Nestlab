b9dc09fb8a81e9f493bfa31dabdd8e0b89330e2d
Anh.Nguyen <salebloomstore@outlook.com>
2026-05-23 14:21:42 +0700

    build

--- example_BE/run.sh
+++ example_BE/run.sh
@@ -114,7 +114,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 
   console.log(`Server running: http://localhost`);
   console.log(`Swagger: http://localhost/swagger`);

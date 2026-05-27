7f31ea905cbc169b871ac4f264e839adb1e765bf
Anh.Nguyen <salebloomstore@outlook.com>
2026-05-28 01:10:07 +0700

    first commit

--- source/src/app.module.ts
+++ source/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const mongoUri = `mongodb://${process.env.MONGO_ADMIN_CONFIG_SERVER}:${process.env.MONGO_PASSWORD_CONFIG_SERVER}@mongos-router-dn:27017,mongos-router-hn:27017,mongos-router-sg:27017/admin?authSource=admin`;
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoUri)],
   controllers: [AppController],
   providers: [AppService],
 })

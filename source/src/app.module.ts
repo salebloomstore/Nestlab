7690ed152202344f9090ca43f2edc11bff4a903d
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-01 17:44:52 +0700

    first commit

--- source/src/app.module.ts
+++ source/src/app.module.ts
@@ -2,11 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-
-const mongoUri = `mongodb://${process.env.MONGO_ADMINIST_SHARDE_MATTER}:${process.env.MONGO_PASSWORD_CONFIG_SERVER}@${process.env.MONGOS_ROUTER_DN_ADRS}:${process.env.PORT_OF_MONGOS},${process.env.MONGOS_ROUTER_HN_ADRS}:${process.env.PORT_OF_MONGOS},${process.env.MONGOS_ROUTER_SG_ADRS}:${process.env.PORT_OF_MONGOS}/${process.env.MONGO_DATABASE_CONFIG_SERVER}?authSource=admin`;
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(mongoUri)],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get(
+          'MONGO_ADMINIST_SHARDE_MATTER',
+        )}:${configService.get(
+          'MONGO_PASSWORD_SHARDE_MATTER',
+        )}@${configService.get(
+          'MONGOS_ROUTER_DN_ADRS',
+        )}:${configService.get('PORT_OF_MONGOS')},${configService.get(
+          'MONGOS_ROUTER_HN_ADRS',
+        )}:${configService.get('PORT_OF_MONGOS')},${configService.get(
+          'MONGOS_ROUTER_SG_ADRS',
+        )}:${configService.get('PORT_OF_MONGOS')}/${configService.get(
+          'MONGO_DATABASE_SHARDE_MATTER',
+        )}?authSource=admin`,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

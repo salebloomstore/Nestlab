a91f662ba592f42837522c14a6f4d0a5a6d389c9
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-01 18:17:20 +0700

    first commit

--- source/src/main.ts
+++ source/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   // SWAGGER
   const config = new DocumentBuilder()
@@ -15,7 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(configService.get<string>('PORT_OF_NESTJS')!);
 
   console.log(`Server running: http://localhost`);
   console.log(`Swagger: http://localhost/swagger`);

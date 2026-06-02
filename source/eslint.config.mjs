acade7a7c5a09e25d67a47cb62b3e7d3e2f4bf33
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-02 21:13:25 +0700

    first commit

--- source/eslint.config.mjs
+++ source/eslint.config.mjs
@@ -3,8 +3,9 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig([
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -32,4 +33,4 @@ export default tseslint.config(
       "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
   },
-);
+])

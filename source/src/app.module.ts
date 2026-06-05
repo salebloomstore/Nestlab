9af1df0a63a72979551a635ecedc2d717fab4a91
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-05 09:48:45 +0700

    first commit

--- source/src/app.module.ts
+++ source/src/app.module.ts
@@ -13,25 +13,19 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
-        const uri = `mongodb://${config.getOrThrow<string>(
-          'MONGO_ADMINIST_SHARDE_MATTER',
-        )}:${config.getOrThrow<string>(
-          'MONGO_PASSWORD_SHARDE_MATTER',
-        )}@${config.getOrThrow<string>(
-          'MONGOS_ROUTER_DN_ADRS',
-        )}:${config.getOrThrow<string>(
-          'PORT_OF_MONGOS',
-        )},${config.getOrThrow<string>(
-          'MONGOS_ROUTER_HN_ADRS',
-        )}:${config.getOrThrow<string>(
-          'PORT_OF_MONGOS',
-        )},${config.getOrThrow<string>(
-          'MONGOS_ROUTER_SG_ADRS',
-        )}:${config.getOrThrow<string>(
-          'PORT_OF_MONGOS',
-        )}/${config.getOrThrow<string>(
-          'MONGO_DATABASE_SHARDE_MATTER',
-        )}?authSource=admin`;
+        const admi = config.getOrThrow<string>('MONGO_ADMINIST_SHARDE_MATTER');
+        const pass = config.getOrThrow<string>('MONGO_PASSWORD_SHARDE_MATTER');
+
+        const hsts = [
+          config.getOrThrow<string>('MONGOS_ROUTER_DN_ADRS'),
+          config.getOrThrow<string>('MONGOS_ROUTER_HN_ADRS'),
+          config.getOrThrow<string>('MONGOS_ROUTER_SG_ADRS'),
+        ];
+
+        const ptdf = config.getOrThrow<string>('PORT_OF_MONGOS');
+        const data = config.getOrThrow<string>('MONGO_DATABASE_SHARDE_MATTER');
+
+        const uri = `mongodb://${admi}:${pass}@${hsts.map((hee) => `${hee}:${ptdf}`).join(',')}/${data}?authSource=admin`;
 
         return { uri };
       },

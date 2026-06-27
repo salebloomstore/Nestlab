e8f289b64ae367994f58bd81925a75f59585bb52
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-28 01:00:33 +0700

    first commit

--- source/src/app.module.ts
+++ source/src/app.module.ts
@@ -35,13 +35,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
         const uri = `mongodb://${admi}:${pass}@${wukong.map((king) => `${king.hsts}:${king.ptdf}`).join(',')}/${data}?authSource=admin`;
 
-        console.log({
-          admi,
-          pass,
-          data,
-          uri,
-        });
-
         return { uri };
       },
     }),

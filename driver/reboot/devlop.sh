9cd5c2728b20b542396577d91ca43ccc54304e97
Anh.Nguyen <salebloomstore@outlook.com>
2026-06-27 23:39:45 +0700

    first commit

--- driver/reboot/devlop.sh
+++ driver/reboot/devlop.sh
@@ -4,7 +4,7 @@ set -e
 
 find . -type f -name "*.sh" -exec chmod +x {} \;
 
-chmod +x driver/shutdown/master.sh && sudo driver/shutdown/master.sh
-chmod +x driver/boot/master.sh && sudo driver/boot/master.sh
+chmod +x driver/shutdown/devlop.sh && sudo driver/shutdown/devlop.sh
+chmod +x driver/boot/devlop.sh && sudo driver/boot/devlop.sh
 
 echo "Application reboot successfully!"

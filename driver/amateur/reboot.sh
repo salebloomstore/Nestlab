92140876bbc15d0fe10fa996607f4b2221a82075
Anh.Nguyen <salebloomstore@outlook.com>
2026-07-04 01:54:57 +0700

    first commit

--- driver/amateur/reboot.sh
+++ driver/amateur/reboot.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 
 find . -type f -name "*.sh" -exec chmod +x {} \;
 
-chmod +x ../shutdown/amateur.sh && sudo ../shutdown/amateur.sh
-chmod +x ../boot/amateur.sh && sudo ../boot/amateur.sh
+chmod +x shutdown.sh && sudo ./shutdown.sh
+chmod +x boot.sh && sudo ./boot.sh
 
 echo "Application reboot successfully!"
